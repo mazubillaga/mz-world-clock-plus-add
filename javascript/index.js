d77097517e26eb53a8791b0295946ce1a515f73c
mazubillaga <mazubillaga03@gmail.com>
2025-05-12 09:16:54 -0700

    quick update

--- javascript/index.js
+++ javascript/index.js
@@ -13,11 +13,14 @@ function updateTime() {
   //Paris
   let parisElement = document.querySelector("#paris");
   if (parisElement) {
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-  parisDateElement.innerHTML = parisTime.format(" MMMM Do, YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format(" MMMM Do, YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
