4b95dbf97cfae11a59caa98ba1f33eef379ec720
mazubillaga <mazubillaga03@gmail.com>
2025-05-12 11:41:54 -0700

    added new city to HTML and JS

--- javascript/index.js
+++ javascript/index.js
@@ -21,6 +21,17 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+    sydneyDateElement.innerHTML = sydneyTime.format(" MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
